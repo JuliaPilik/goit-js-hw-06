96e1929844fbb0fbf02685a60c0bf9a68cde4019
JuliaPilik <109143460+JuliaPilik@users.noreply.github.com>
2022-11-14 22:54:45 +0200

    изменяет 1 задание

--- js/task-01.js
+++ js/task-01.js
@@ -1,4 +1,5 @@
 const amountCategories = document.querySelectorAll('li.item');
+console.log(`Number of categories: ${amountCategories.length}`);
 amountCategories.forEach(function (amountCategory, index) {
     console.log(`Category: ${amountCategory.firstElementChild.textContent}`);
     console.log(`Elements: ${amountCategory.lastElementChild.children.length}`);
